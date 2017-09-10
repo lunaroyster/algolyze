359a2f21092cfd08f6ecb591936aa045e2c9abfc
lunaroyster <oneinaquadrillion@live.com>
2017-09-10 02:48:33 +0000

    Initialized public app.js

---
+++ public/app.js
@@ -0,0 +1,21 @@
+/* global angular */
+
+var app = angular.module("algolyze", ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+    
+    .when('/', {
+        controller  : 'searchController',
+        templateUrl : './views/search.html'
+    });
+    
+}]);
+
+app.controller('homeController', function($scope) {
+    
+});
+
+app.controller('searchController', function($scope) {
+    
+});
