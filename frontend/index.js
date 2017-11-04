d571d126cbbf2b345614ab631279d003ddfd4d9a
lunaroyster <oneinaquadrillion@live.com>
2017-11-04 09:15:36 +0000

    less compiler

--- frontend/index.js
+++ frontend/index.js
@@ -2,5 +2,5 @@ const fse = require('fs-extra');
 
 module.exports = (async function() {
     await fse.emptyDirSync('./frontend/generated');
-    await require('./compileless');
+    await require('./compileless')(`${__dirname}/less/`, `${__dirname}/generated/css/`);
 })();
