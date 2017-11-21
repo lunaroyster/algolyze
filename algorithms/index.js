c6a55c24d454e1ee9c578cfec8db22683eb81863
lunaroyster <oneinaquadrillion@live.com>
2017-11-21 19:06:10 +0000

    sitemapping

--- algorithms/index.js
+++ algorithms/index.js
@@ -1,18 +1,21 @@
 const fse = require('fs-extra');
+const sm = require('sitemap')
 
 module.exports = (async function() {
     console.log("Compiling data.");
     
     let dir = __dirname;
-    await fse.emptyDir(`${dir}/generated/algorithms/`); 
-        
+    await fse.emptyDir(`${dir}/generated/algorithms/`);
+    await fse.emptyDir(`${dir}/generated/sitemaps/`);
+    
     let algorithms = [];
+    let algorithmURLs = [];
     let algorithmNames = await fse.readdir(`${dir}/algorithms`);
     for(let algorithmName of algorithmNames) {
         let algorithmFile = await fse.readJSON(`${dir}/algorithms/${algorithmName}/main.json`);
-        let algorithmPage;
+        algorithmURLs.push(algorithmFile.url);
         try {
-            algorithmPage = await fse.readFile(`${dir}/algorithms/${algorithmName}/page.md`, 'utf8');
+            let algorithmPage = await fse.readFile(`${dir}/algorithms/${algorithmName}/page.md`, 'utf8');
             algorithmFile.longDescription = algorithmPage;
         }
         catch (error) {
@@ -23,6 +26,9 @@ module.exports = (async function() {
     await fse.outputJSON(`${dir}/generated/algorithms/compiled.json`, algorithms);
     
     await fse.copy(`${dir}/algorithms`, `${dir}/generated/algorithms`, {overwrite: false});
+    
+    let sitemap = sm.createSitemap({hostname: 'https://algolyze.me/#!/a', urls: algorithmURLs});
+    await fse.writeFile(`${dir}/generated/sitemaps/algorithms.xml`, sitemap.toXML(), 'utf8');
     
     console.log("Compiled data.");
 })();
