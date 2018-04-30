a3e5d067fa915f0049db7890b7e656f42f2a68a1
lunaroyster <oneinaquadrillion@live.com>
2018-04-30 11:48:17 +0000

    Removed old.

--- frontend/index.js
+++
@@ -1,8 +0,0 @@
-const fse = require('fs-extra');
-
-module.exports = (async function() {
-    await fse.emptyDirSync('./frontend/generated');
-    // await require('./compileless')(`${__dirname}/less/`, `${__dirname}/generated/css/`);
-    await require('./compilescss')(`${__dirname}/scss/`, `${__dirname}/generated/css/`);
-    await require('./babelcompile')(`${__dirname}/static/app.js`, `${__dirname}/generated/app41.js`);
-})();
--- tests/generators/index.js
+++
@@ -1,1 +0,0 @@
-module.exports.AlgorithmGenerator = require('./algorithmGenerator');
